ad41f927a6ed900005080c3cc66643eaa3693806
Jonathan Tapia <jonathan.tapia@crowdint.com>
2014-02-25 13:22:12 -0600

    Update scope to avoid warning

--- lib/opinio/opinio_subjectum.rb
+++ lib/opinio/opinio_subjectum.rb
@@ -1,24 +1,20 @@
 module Opinio
   module OpinioSubjectum
-  
+
     def self.included(base)
       base.extend(ClassMethods)
     end
-  
+
     module ClassMethods
       def opinio_subjectum(*args)
         options = args.extract_options!
         options.delete(:as)
 
-        default_options = { :class_name => Opinio.model_name,
-                            :as => :commentable,
-                            :order => "created_at #{Opinio.sort_order}",
-                            :dependent => :destroy }
-
-        has_many :comments, default_options.merge(options)
+        has_many :comments, -> { order(options[:order]) },
+          :as => :commentable, :dependent => :destroy, :class_name => Opinio.model_name
 
       end
     end
-  
+
   end
 end
